9fdf29a6c14b5d99afaee4d9c4b98d6b6c19aaaa
torero7 <sk.torero7@gmail.com>
2014-02-07 00:16:57 -0800

    Update testparcing.php

--- testparcing.php
+++ testparcing.php
@@ -1,7 +1,8 @@
 <?php
-//version 0.4
+//version 0.6
 $dest_month = "12";
 $dest_year = "2013";
+$telnum = "71012";
 $dest_dir= "C:\\wamp\\www\\smdr\\".$dest_year."\\";
 $dest_dir=$dest_dir.$dest_month."\\";
 $log_file = $dest_dir."smdr_".$dest_month."_".$dest_year.".log";
@@ -9,6 +10,7 @@ $bee_file = "C:\\wamp\\www\\beeline\\".$dest_year."\\HF975_".$dest_month.".TXT";
 
 function findcost($number)
 {
+//version 0.2
 global $bee_file;
 $number=substr_replace($number, '7', 0, 1);
 $handle = @fopen($bee_file, "r");
@@ -20,42 +22,16 @@ $find = 0;
 	{
 $buffer = preg_replace('/[\s]{2,}/', ' ', $buffer);
 $dest_array = explode(" ",$buffer);
-			
-//print_r("=".$number."=\n");
-//print_r("=".$dest_array[6]."=\n");
 if($counter>=11)$findnum=substr_count($dest_array[6],$number);
-//print_r($findnum."=\n");
-//print_r(array_values($dest_array));
-/*
-$dest_array = array_values($dest_array);
-foreach (array_values($itemsForDelete) as $deleteIndex) {
-    unset ($dest_array[$deleteIndex]);  }
-$dest_array = array_values($dest_array);
-*/
-// and $counter++ >=10 and $find<1 and $dest_array[11]!='0:00'     
-		if($counter++ >=11 and $findnum >0)
+		if($counter++ >=11 and $findnum >0 and $dest_array[3]!='0:00')
 			{       
-//print_r(array_values($dest_array));
 				$cost1 =explode(".", $dest_array[4]);
 				$cost = $cost1[0]+$cost1[1]/100;
 				$minutes1 =explode(":", $dest_array[3]);
 				$minutes = (int) $minutes1[0];
 				$sum_cost = $cost/$minutes;
 				$find++;
-/*
-	echo   $minutes."---".$cost."---------".$sum_cost."\n";
-echo 	 "0-".$dest_array[0]."\n"
-	."1-".$dest_array[1]."\n"
-	."2-".$dest_array[2]."\n"
-	."3-".$dest_array[3]."\n"
-	."4-".$dest_array[4]."\n"
-	."5-".$dest_array[5]."\n"
-	."6-".substr_replace($dest_array[19], '8', 0, 1)."\n"
-
-                        ;
-*/				
 			}
-//break;
     	}
     if (!feof($handle)) {
         echo "Error: unexpected fgets() fail\n";
@@ -74,7 +50,7 @@ $counter = 0;
 	{
 		$counter_cost = 0.00;
 		$dest_array = explode(",",$buffer);
-		if($counter++ >=2 and $dest_array[3]=='71033' and $dest_array[8] !=='1' and $dest_array[1] !=='00:00:00' and strlen($dest_array[6]) > 8) 
+		if($counter++ >=2 and $dest_array[3]==$telnum and $dest_array[8] !=='1' and $dest_array[1] !=='00:00:00' and strlen($dest_array[6]) > 8) 
 
 			{       $min = $dest_array[1];
 				$call_time = explode(":", $min);
